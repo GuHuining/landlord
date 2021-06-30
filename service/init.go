947ae23ae46f23b60ed1b57cf00d0df38e064b0e
305579164@qq.com <305579164@qq.com>
2021-06-30 22:47:47 +0800

    [fix]:

--- service/init.go
+++ service/init.go
@@ -7,11 +7,15 @@ import (
 	"github.com/gin-contrib/sessions/cookie"
 )
 
-// Set global session
 var Store sessions.Store
 
 func init() {
-	// Initiate session by using crypto/rand to make sure the key is random.
+	initSession()
+}
+
+// 初始化session
+func initSession() {
+	// 随机生成session密钥
 	key := make([]byte, 64)
 	_, err := rand.Read(key)
 	if err != nil {
