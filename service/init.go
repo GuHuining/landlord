c325b30dbd4e5f21f2c24bae2fc149fa3ecefbc4
305579164@qq.com <305579164@qq.com>
2021-07-02 16:20:02 +0800

    [fix]

--- service/init.go
+++ service/init.go
@@ -3,9 +3,6 @@ package service
 import (
 	"crypto/rand"
 
-	"landlord/config"
-	"landlord/hub"
-
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 )
@@ -14,7 +11,6 @@ var Store sessions.Store
 
 func init() {
 	initSession()
-	initRooms()
 }
 
 // 初始化session
@@ -26,27 +22,4 @@ func initSession() {
 		panic(err)
 	}
 	Store = cookie.NewStore(key)
-}
-
-var emptyRooms, roomWithPassword, roomWithoutPassword, roomWithPasswordPlaying, roomWithoutPasswordPlaying *hub.Rooms
-
-// 初始化房间
-func initRooms() {
-	emptyRooms.New()
-	roomWithPassword.New()
-	roomWithoutPassword.New()
-	roomWithPasswordPlaying.New()
-	roomWithoutPasswordPlaying.New()
-
-	playConfig := config.GetPlayConfig()
-
-	// 初始化空房间
-	for i := playConfig.Rooms.Number; i > 1; i-- {
-		var room = hub.Room{
-			ID: i,
-		}
-		room.New()
-		emptyRooms.PushBack(&room)
-	}
-
 }
